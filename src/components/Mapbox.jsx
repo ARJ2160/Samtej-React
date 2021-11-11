ebc19f1989d08346b5a17d7df3ac400d6d4973a2
Atharva Joshi <atharvaj2160@gmail.com>
2021-11-11 13:58:10 +0530

    Added Product Section/ Bing Maps

--- src/components/Mapbox.jsx
+++ src/components/Mapbox.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useRef, useState } from 'react'
+// import React, { useEffect, useRef, useState } from 'react'
 // import 'mapbox-gl/dist/mapbox-gl.css';
 // import mapboxgl from 'mapbox-gl';
 import "../index.css"
